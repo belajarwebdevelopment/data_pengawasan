5494aa92d099979bcea13605cf268b33c7d40faa
belajarwebdevelopment <belajar.webdevelopment@gmail.com>
2025-06-29 16:28:37 +0700

    aplikasi download wtu data 2

--- index.js
+++ index.js
@@ -1,13 +1,98 @@
-function pilihKelurahanHandle() {
+let kelurahan = '';
+
+function pilihKelurahanHandler() {
 	document.getElementById("kelurahan").addEventListener("change",function() {
-		console.log(this.value);
+		
+		kelurahan = this.value;
 	});
 }
+
+function clickTombolHandler() {
+	document.querySelectorAll('.btn').forEach(btn => {
+		btn.addEventListener("click", async function() {
+			
+			this.textContent = `Sending request .... please wait.`;
+			this.style.backgroundColor = "#888";
+
+			await getDataWtu_or_koordinat(this.id);
+			
+			this.id === "wtu" ? this.textContent = `UNDUH DATA WTU` : this.textContent = `UNDUH DATA KOORDINAT`;
+			this.id === "wtu" ? this.style.backgroundColor = "#b3cc33" : this.style.backgroundColor = "#34495e";
+		});
+	});
+}
+
+function closeTemplate() {
+	document.getElementById("tutup").addEventListener("click", () => {
+		document.querySelector(".dataTemplate").style.display = "none";
+	});
+}
+
+
+function dataTemplateOpen(judul) {
+	document.getElementById("seeData").addEventListener("click", () => {
+		document.querySelector(".dataTemplate").style.display = "block";
+		document.getElementById("judul").innerHTML = judul;
+		closeTemplate();
+	});
+}
+
+async function getDataWtu_or_koordinat(menu) {
+	//let api = "https://script.google.com/macros/s/AKfycbxMcX8HycMduVzPOTb3kRJxJ-dOxKslvqHfKtkWd7BgHr2FWVFSM_nHmzI57ks5Fuk0/exec";
+	let api = "https://script.google.com/macros/s/AKfycbxXxh8VxpYZqDZvw1KCJBUKVQkDeDAvlcxEf2X9pSWMO0FFJBLBCrymTnu3V4LLBBT3/exec";
+
+	let box = document.querySelector(".downloadBox");
+	box.style.display = "none";
+	let strTable = ``;
+	
+	await fetch(`${api}?kelurahan=${kelurahan}&menu=${menu}`,{
+		method : 'POST'
+	}).then(data => data.json()).then(data => {
+		console.log(data.datas);
+
+		box.style.display = "block";
+		box.innerHTML = `
+			<a href="${data.downloadUrl}" class="linkdl" target="_blank">Click to download -> ${menu}_${kelurahan}.csv</a><br>
+			<a href=# class="linkdl" id="seeData">See The Datascsv</a>
+		`;
+
+		dataTemplateOpen(`Data ${menu} Kelurahan ${kelurahan}`);
+
+		data.datas.forEach(item => {
+			strTable += `
+				<tr>
+					<td>${item[0]}</td>
+					<td>${item[1]}</td>
+					<td>${item[2]}</td>
+					<td>${item[3]}</td>
+					<td>${item[4]}</td>
+					<td>${item[5]}</td>
+					<td>${item[6]}</td>
+					<td>${item[7]}</td>
+					<td>${item[8]}</td>
+					<td>${item[9]}</td>
+					<td>${item[10] === "CC" ? '' : item[10]}</td>
+					<td>${item[11]}</td>
+					<td>${item[12]}</td>
+				</tr>
+			`;
+		});
+		document.getElementById('tableBody').innerHTML = strTable;
+	});
+	
+}
+
 
 (async function main() {
 	let api = "https://script.google.com/macros/s/AKfycbxxWf-43TUg5XvBOes_r89pANhi6fCOQgqhF_SwEmjJMWSIvv9BSDEsX2ZCpUGQDMRg/exec";
 
-	let str = `<select id="kelurahan" class="minimalist-orange-select">`;
+	let str = `
+	    <div class="select-wrapper">
+	        <div class="select-container">
+	            <select id="kelurahan" class="styled-select">
+    `;
+
+
 	await fetch(api).then(response => response.json()).then(data => {
 		
 		data.data.forEach((el,idx) => {
@@ -16,11 +101,15 @@ function pilihKelurahanHandle() {
 		});
 	});
 
-	str += `</select>`;
+	str += `	</select>
+	        </div>
+   		</div>
+   	`;
 
-	document.querySelector(".sub-header").innerHTML = `Kelurahan ${str}`;
+	document.querySelector(".sub-header").innerHTML = `${str}`;
 
-	pilihKelurahanHandle();
+	pilihKelurahanHandler();
+	clickTombolHandler();
 
 
 
